236e08ee8f1827d5734cfc49463095ee785600de
JulioVanzelli <vanzelliver@gmail.com>
2019-06-08 16:57:22 -0300

    Implementação da leitura de temperatura.

---
+++ app/src/main/java/br/com/juliovanzelli/TemperaturaActivity.kt
@@ -0,0 +1,34 @@
+package br.com.juliovanzelli
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import com.google.firebase.database.*
+import kotlinx.android.synthetic.main.activity_iluminacao.*
+import kotlinx.android.synthetic.main.activity_temperatura.*
+
+class TemperaturaActivity : AppCompatActivity() {
+
+    private lateinit var database: FirebaseDatabase
+    private lateinit var temperaturaRef: DatabaseReference
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_temperatura)
+
+        database = FirebaseDatabase.getInstance()
+        temperaturaRef = database.getReference("temperatura")
+
+        temperaturaRef.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                val value = dataSnapshot.getValue(Double::class.java)
+
+                tvTemperatura.setText(value.toString())
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+
+            }
+        })
+    }
+}
